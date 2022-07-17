306e3948c8b799cb85cf48002b0f439950fc3576
DevBorisElkin <ea.foreverrr@gmail.com>
2022-07-17 23:54:45 +0300

    backup

---
+++ VkNewsFeed/Services/NetworkService.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkService.swift
+//  VkNewsFeed
+//
+//  Created by test on 17.07.2022.
+//
+
+import Foundation
+
+final class NetworkService{
+    getFeed(){
+        var components = URLComponents()
+        components.scheme
+        components.host
+        components.path
+        components.queryItems
+        
+        components.url
+    }
+}
