da1d310ffb3cdc16ccba76fc2ff6823f28aa1546
DevBorisElkin <ea.foreverrr@gmail.com>
2022-07-22 18:29:16 +0300

    parsing data from get request

--- VkNewsFeed/FeedViewController/FeedViewController.swift
+++ VkNewsFeed/FeedViewController/FeedViewController.swift
@@ -10,27 +10,26 @@ import UIKit
 
 class FeedViewController: UIViewController{
     
-    private let networkService: Networking = NetworkService()
+    private var fetcher: DataFetcher = NetworkDataFetcher(networking: NetworkService())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .blue
         
-        let params = ["filters" : "post, photo"]
-        networkService.request(path: API.newsfeed, params: params) { data, error in
-            if let error = error {
-                print("Error received requesting data: \(error.localizedDescription)")
-                return
-            }
+        fetcher.getFeed { feedResponse in
+            guard let feedResponse = feedResponse else { return }
             
-            guard let data = data else {
-                print("Something is wrong with data")
-                return
+            feedResponse.items.map { feedItem in
+                print(feedItem.date)
             }
-            
-            var json = try? JSONSerialization.jsonObject(with: data, options: [])
-            print("json: \(json!)")
         }
     }
 }
+
+//            response?.response.items.map({ feedItem in
+//                print(feedItem.text)
+//            })
+            
+//            var json = try? JSONSerialization.jsonObject(with: data, options: [])
+//            print("json: \(json!)")
---
+++ VkNewsFeed/Services/Models/FeedResponse.swift
@@ -0,0 +1,32 @@
+//
+//  FeedResponse.swift
+//  VkNewsFeed
+//
+//  Created by test on 22.07.2022.
+//
+
+import Foundation
+
+struct FeedResponseWrapped: Decodable{
+    let response: FeedResponse
+}
+
+struct FeedResponse: Decodable{
+    var items: [FeedItem]
+}
+
+struct FeedItem: Decodable{
+    let sourceId: Int
+    let postId: Int
+    let text: String?
+    let date: Double
+    let comments: CountableItem?
+    let likes: CountableItem?
+    let reposts: CountableItem?
+    let views: CountableItem?
+}
+
+struct CountableItem: Decodable{
+    let count: Int
+    
+}
