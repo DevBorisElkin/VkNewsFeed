0c9c7c13eb459a554e154ca15f160f152c6d02ae
DevBorisElkin <ea.foreverrr@gmail.com>
2022-07-28 22:42:05 +0300

    basic news cells via xib file

---
+++ VkNewsFeed/NewsFeed/Cell/NewsfeedCell.swift
@@ -0,0 +1,31 @@
+//
+//  NewsfeedCell.swift
+//  VkNewsFeed
+//
+//  Created by test on 28.07.2022.
+//
+
+import Foundation
+import UIKit
+
+class NewsfeedCell: UITableViewCell {
+    
+    static let reuseId = "NewsfeedCell"
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+    
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var postLabel: UILabel!
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var commentsLabel: UILabel!
+    @IBOutlet weak var sharesLabel: UILabel!
+    @IBOutlet weak var viewsLabel: UILabel!
+    
+    
+}
